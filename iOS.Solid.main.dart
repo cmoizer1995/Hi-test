e80cd607df70a9c3ccb3d124f896489668392451
Connor  Moizer <85117462+cmoizer1995@users.noreply.github.com>
2026-09-05 15:04:48 +0100

    Create iOS.Solid.main.dart

---
+++ iOS.Solid.main.dart
@@ -0,0 +1,23 @@
+// platform: ios
+// status-bar: solid
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('CM Flutter')),
+        body: const Center(
+          child: Text('Hello from Flutter'),
+        ),
+      ),
+    );
+  }
+}
